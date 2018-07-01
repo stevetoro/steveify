88d5d85ba64bf380aadde62badbc154152e1ee21
Steve Toro <steve@stevetoro.com>
2018-06-30 21:41:32 -0400

    Search works 🔥

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,51 +2,20 @@ import React, { Component } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../utils/Spotify';
 import './App.css';
-
-const searchTerm = "";
-const playlistName = "Nas Playlist";
-
-const searchResults = [
-  {
-  id: 0,
-  name: "Not for Radio",
-  artist: "Nas",
-  album: "Nasir"
-  },
-  {
-  id: 1,
-  name: "Cops Shot the Kid",
-  artist: "Nas",
-  album: "Nasir"
-  },
-  {
-  id: 2,
-  name: "White Label",
-  artist: "Nas",
-  album: "Nasir"
-  }
-];
-
-const playlistTracks = [
-  {
-  id: 3,
-  name: "The World is Yours",
-  artist: "Nas",
-  album: "Illmatic"
-  },
-];
 
 class App extends Component {
   state = {
-    searchTerm,
-    searchResults,
-    playlistName,
-    playlistTracks
+    searchTerm: '',
+    searchResults: [],
+    playlistName: 'New Playlist',
+    playlistTracks: []
   }
 
-  search = () => {
-    console.log(this.state.searchTerm);
+  search = async () => {
+    const searchResults = await Spotify.search(this.state.searchTerm);
+    this.setState({ searchResults });
   }
 
   addTrack = track => () => {
--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,4 +1,5 @@
 import { CLIENT_ID, REDIRECT_URI } from './config';
+const searchEndpoint = `https://api.spotify.com/v1/search?`;
 
 let accessToken = '';
 
@@ -19,6 +20,24 @@ const Spotify = {
     }
 
     window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+  },
+
+  search: async function (term) {
+    const accessToken = this.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    const response = await fetch(`${searchEndpoint}type=track&q=${term}`, { headers });
+    const json = await response.json();
+
+    if (json.tracks) 
+      return json.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+
+    return [];
   }
 };
 
