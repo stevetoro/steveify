ca916726647201b0238c7bd59db5632e0f5dac64
Steve Toro <steve@stevetoro.com>
2020-12-11 19:11:56 -0500

    Removed class component import from functional components.

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React from 'react';
 import Tracklist from '../Tracklist/Tracklist';
 import './SearchResults.css';
 
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,4 +1,4 @@
-import React , { Component } from 'react';
+import React from 'react';
 import './Track.css';
 
 const Track = ({ track, action, removable }) => (
