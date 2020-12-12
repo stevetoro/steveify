308e45695ba1402ed183e15494f2d61d337469fa
Steve Toro <steve@stevetoro.com>
2020-12-12 01:03:18 -0500

    Using sessionStorage inside effect hook to store searchTerm between redirects.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './SearchBar.css';
 
 const SearchBar = ({ searchSpotify }) => {
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    const storedSearchTerm = sessionStorage.getItem('searchTerm')
+    setSearchTerm(storedSearchTerm)
+  }, [])
+
+  useEffect(() => {
+    sessionStorage.setItem('searchTerm', searchTerm)
+  }, [searchTerm])
 
   const handleSearchTermChange = ({ target }) => setSearchTerm(target.value)
 
